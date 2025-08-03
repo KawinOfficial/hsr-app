ea481c4c2848947f68e89bae5389feccde12a909
KawinSrisomphan <kawin@harmonyx.co>
2025-08-03 14:55:32 +0700

    feat: integrate ky instead of fetch on api connection

--- src/features/auths/hook/use-users.ts
+++ src/features/auths/hook/use-users.ts
@@ -1,17 +1,10 @@
+import { api } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 
 export const useUsers = () => {
   return useQuery({
     queryKey: ["users"],
-    queryFn: async () => {
-      const res = await fetch("/api/users");
-
-      if (!res.ok) {
-        const errorData = await res.json();
-        throw new Error(errorData.error || "Failed to fetch users");
-      }
-      return res.json();
-    },
+    queryFn: async () => await api.get("users").json(),
     retry: false,
   });
 };
