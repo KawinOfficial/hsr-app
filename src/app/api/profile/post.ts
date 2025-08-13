a1c5a820be25c2b1de78793d3d78d462be79d6ae
KawinSrisomphan <kawin@harmonyx.co>
2025-08-13 20:24:51 +0700

    refactor: profile API

---
+++ src/app/api/profile/post.ts
@@ -0,0 +1,60 @@
+import { supabase } from "@/lib/supabase";
+import { checkUserAuth } from "@/lib/promise";
+import { NextResponse } from "next/server";
+
+export async function updateProfile(req: Request) {
+  try {
+    const userId = await checkUserAuth();
+    const body = await req.json();
+    const {
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      nationality,
+      otherNationality,
+      employeeInfo = {},
+    } = body;
+
+    const { error: userUpdateError } = await supabase
+      .from("User")
+      .update({
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        nationality,
+        otherNationality,
+      })
+      .eq("id", userId);
+    if (userUpdateError) {
+      throw new Error(userUpdateError.message);
+    }
+
+    const { employeeId, position, department, managerName, workLocation } =
+      employeeInfo;
+    const { error: empUpdateError } = await supabase
+      .from("EmployeeInfo")
+      .update({
+        employeeId,
+        position,
+        department,
+        managerName,
+        workLocation,
+      })
+      .eq("userId", userId);
+
+    if (empUpdateError) {
+      throw new Error(empUpdateError.message);
+    }
+
+    return NextResponse.json({
+      ...body,
+      status: "active",
+    });
+  } catch (error) {
+    throw new Error(
+      error instanceof Error ? error.message : "Internal server error"
+    );
+  }
+}
